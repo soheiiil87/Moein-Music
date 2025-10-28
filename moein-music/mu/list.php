1c6827566ea53213c99f0d875951d390741d97ab
soheil <soheil2022ta@gmail.com>
2025-10-28 08:19:48 +0330

    Delete moein-music/mu/list.php

--- moein-music/mu/list.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dir = __DIR__;
-$files = array_values(array_filter(scandir($dir), function($file) {
-    return pathinfo($file, PATHINFO_EXTENSION) === 'mp3';
-}));
-header('Content-Type: application/json');
-echo json_encode($files);
-?>
